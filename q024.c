3a08d1818b72883906fa498d17cc31e4a4800f4d
het <hetkyada0292@gmail.com>
2026-03-25 13:18:58 +0530

    Delete First Occurrence of a Key

---
+++ q024.c
@@ -0,0 +1,100 @@
+// Problem: Delete First Occurrence of a Key - Implement using linked list with dynamic memory allocation.
+
+// Input:
+// - First line: integer n
+// - Second line: n space-separated integers
+// - Third line: integer key
+
+// Output:
+// - Print the linked list elements after deletion, space-separated
+
+// Example:
+// Input:
+// 5
+// 10 20 30 40 50
+// 30
+
+// Output:
+// 10 20 40 50
+
+// Explanation:
+// Traverse list, find first node with key, remove it by adjusting previous node's next pointer.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+struct Node* insertEnd(struct Node* head, int data) {
+    struct Node* newNode = createNode(data);
+    if (head == NULL)
+        return newNode;
+
+    struct Node* temp = head;
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = newNode;
+    return head;
+}
+
+struct Node* deleteKey(struct Node* head, int key) {
+    if (head == NULL)
+        return head;
+
+    if (head->data == key) {
+        struct Node* temp = head;
+        head = head->next;
+        free(temp);
+        return head;
+    }
+
+    struct Node* temp = head;
+    while (temp->next != NULL && temp->next->data != key)
+        temp = temp->next;
+
+    if (temp->next != NULL) {
+        struct Node* delNode = temp->next;
+        temp->next = delNode->next;
+        free(delNode);
+    }
+
+    return head;
+}
+
+void printList(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+}
+
+int main() {
+    int n, x, key;
+    struct Node* head = NULL;
+
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        head = insertEnd(head, x);
+    }
+
+    scanf("%d", &key);
+
+    head = deleteKey(head, key);
+
+    printList(head);
+
+    return 0;
+}
