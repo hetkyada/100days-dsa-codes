31a1840f65efe0ecf4281b3b4e728786cda9c643
hetkyada <hetkyada0292@gmail.com>
2026-05-04 10:15:13 +0530

    Heap Sort Using Max-Heap

--- q040.c
+++ q040.c
@@ -1,5 +1,5 @@
 /*
-Day 40: Heap Sort Using Max-Heap
+Day 40: Heap Sort Using Max-Heap 
 
 Implement Heap Sort using a Max Heap to sort an array in ascending order. First build a max heap, then repeatedly extract the maximum element and place it at the end of the array.
 */
