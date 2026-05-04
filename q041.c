c571037c645e1fb368a959515e1704ee44a4cf1b
hetkyada <hetkyada0292@gmail.com>
2026-05-04 10:15:30 +0530

    Queue Using Linked List

--- q041.c
+++ q041.c
@@ -7,7 +7,7 @@ Implement a Queue using a linked list supporting enqueue and dequeue operations.
 Input Format:
 - First line contains integer N
 - Next N lines contain queue operations
-
+ 
 Output Format:
 - Print dequeued elements
 - Print -1 if dequeue is attempted on an empty queue
